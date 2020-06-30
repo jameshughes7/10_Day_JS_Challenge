8ae9db63473379789e4385e07f1ba09c6eec87b8
jameshughes7 <hugjimbo77@gmail.com>
2020-06-30 15:43:55 +0100

    better solution for alternatingSums challenge

--- src/Day3/alternatingSums.ts
+++ src/Day3/alternatingSums.ts
@@ -1,11 +1,18 @@
 export const alternatingSums = ((array) => {
-    let team1 = [];
-    let team2 = [];
-    let finalArray = [];
+    // let team1 = [];
+    // let team2 = [];
+    // let finalArray = [];
+    // for (let i = 0; i < array.length; i++) {
+    //     i % 2 === 0 ? team1.push(array[i]) : team2.push(array[i]);
+    // }
+    // finalArray.push(team1.reduce((prev,curr) => prev + curr, 0));
+    // finalArray.push(team2.reduce((prev,curr) => prev + curr, 0));
+    // return finalArray;
+
+    let evenSum = 0;
+    let oddSum = 0;
     for (let i = 0; i < array.length; i++) {
-        i % 2 === 0 ? team1.push(array[i]) : team2.push(array[i]);
+        i % 2 === 0 ? evenSum += array[i] : oddSum += array[i];
     }
-    finalArray.push(team1.reduce((prev,curr) => prev + curr, 0));
-    finalArray.push(team2.reduce((prev,curr) => prev + curr, 0));
-    return finalArray;
+    return [evenSum, oddSum];
 })
--- src/Day4/arrayReplace.ts
+++ src/Day4/arrayReplace.ts
@@ -1,7 +1,7 @@
 // Array replace challenge
 // Given an array of integers, replace all the the occurrences of elemToReplace with substitutionElem
 export const arrayReplace = ((inputArray: Array<number>, elemToReplace: number, substitutionElem: number) => {
-    for (let i = 0; i <= inputArray.length-1; i ++) {
+    for (let i = 0; i < inputArray.length; i ++) {
         inputArray[i] === elemToReplace ? inputArray[i] = substitutionElem : inputArray[i];
     }
     return inputArray;
